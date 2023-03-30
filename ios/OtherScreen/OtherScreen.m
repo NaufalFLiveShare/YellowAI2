4c1cd2b62cc998047b74851694f54d87c630ec2d
Muhammad Naufal Rabbani <muhammad.rabbani@flip.id>
2023-03-30 14:35:44 +0700

    fix: ios presentingscreen on top of yellow chat yellow

--- ios/OtherScreen/OtherScreen.m
+++ ios/OtherScreen/OtherScreen.m
@@ -19,13 +19,24 @@ RCT_EXPORT_MODULE();
   return @[];
 }
 
+// references: https://stackoverflow.com/a/29163021 (Find top most view controller in iOS)
+- (UIViewController*) topMostController
+{
+   UIViewController *topController = [UIApplication sharedApplication].keyWindow.rootViewController;
+
+   while (topController.presentedViewController) {
+       topController = topController.presentedViewController;
+   }
+
+   return topController;
+}
+
 RCT_EXPORT_METHOD(showOtherScreen) {
     dispatch_async(dispatch_get_main_queue(), ^{
-      UIViewController *rootViewController = [UIApplication sharedApplication].delegate.window.rootViewController;
-      OtherViewController *otherViewController = [[OtherViewController alloc] init];
-      UINavigationController *navigationController = (UINavigationController *)rootViewController;
-      [navigationController setNavigationBarHidden:NO animated:YES];
-      [navigationController pushViewController:otherViewController animated:YES];
+      OtherViewController *otherViewController = [[OtherViewController alloc]  init];
+      otherViewController.modalPresentationStyle = UIModalPresentationOverFullScreen;
+      UIViewController *topVC = [self topMostController];
+      [topVC presentViewController:otherViewController animated:YES completion:nil];
     });
 }
 
--- ios/OtherScreen/OtherViewController.h
+++ ios/OtherScreen/OtherViewController.h
@@ -9,6 +9,8 @@
 
 @interface OtherViewController : UIViewController
 
+@property (nonatomic, strong) UIButton *closeButton;
+
 @end
 
 
@@ -20,22 +22,42 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
     // Set the background color to white
-    self.view.backgroundColor = [UIColor blackColor];
-  
+     self.view.backgroundColor = [UIColor blackColor];
+   
+    // Create and configure close button
+    self.closeButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [self.closeButton setTitle:@"Close" forState:UIControlStateNormal];
+    [self.closeButton addTarget:self action:@selector(closeButtonTapped) forControlEvents:UIControlEventTouchUpInside];
+    self.closeButton.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:self.closeButton];
+    
+    // Add constraints for the close button
+    [NSLayoutConstraint activateConstraints:@[
+        [self.closeButton.topAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor constant:16],
+        [self.closeButton.trailingAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.trailingAnchor constant:-16],
+        [self.closeButton.widthAnchor constraintEqualToConstant:80],
+        [self.closeButton.heightAnchor constraintEqualToConstant:40]
+    ]];
+    
+    // Create and configure label
     UILabel *label = [[UILabel alloc] init];
     label.text = @"this is other native screen";
     label.textAlignment = NSTextAlignmentCenter;
     label.font = [UIFont systemFontOfSize:24];
     label.translatesAutoresizingMaskIntoConstraints = NO;
-
     [self.view addSubview:label];
-
+    
+    // Add constraints for the label
     [NSLayoutConstraint activateConstraints:@[
         [label.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor],
         [label.centerYAnchor constraintEqualToAnchor:self.view.centerYAnchor]
     ]];
+}
+
+- (void)closeButtonTapped {
+    // Handle button tap event here
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 @end
